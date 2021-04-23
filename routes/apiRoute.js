4409794408ce1a82a5b4908e95c26b0f0aa9c8e3
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-22 21:31:27 -0700

    Finished get route for workouts range

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -25,3 +25,23 @@ router.put('api/workouts/id', ({ body, params }, res) => {
             res.status(400).json(error);
         });
 });
+
+router.get('api/workouts/range', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration'
+                }
+            }
+        }
+    ])
+        .sort({ _id: -1 })
+        .limit(7)
+        .then((workoutDB) => {
+            res.json(workoutDB);
+        })
+        .catch(error => {
+            res.status(400).json(error);
+        });
+});
