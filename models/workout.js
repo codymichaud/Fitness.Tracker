c78f0897cd6dea4c027901c6125ed927811209e5
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-22 22:16:41 -0700

    Working on workout.js model file

--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,39 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: 'Enter the workout name'
+        },
+        type: {
+            type: String,
+            enum: ['Cardio', 'Resistance'],
+            required: 'Choose the type of workout'
+        },
+        weight: {
+            type: Number,
+            required: 'Enter a desired weight'
+        },
+        reps: {
+            type: Number,
+            required: 'Enter desired number of reps'
+        },
+        sets: {
+            type: Number,
+            required: 'Enter desired number of sets'
+        },
+        duration: {
+            typer: Number,
+            required: 'Enter desired length of workout in minutes'
+        },
+
+
+    }]
+})
